e74d5f0ea07f48aecdf1172e4e4877cbe2143c3e
MateusChavito <mateus.chavito@hotmail.com>
2024-06-10 22:19:27 -0300

    Tarefa de calculo médio de 4 notas

---
+++ 4notasMedia/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        leituraNotas();
+    }
+
+        public static void leituraNotas () {
+            System.out.println("**** leituraNotas ****");
+            float num1 = 9;
+            float num2 = 8;
+            float num3 = 6;
+            float num4 = 4;
+            float media = (num1 + num2 + num3 + num4) /4;
+            System.out.println(media);
+
+// não estava conseguindo gerar a média pois o 4 estava dentro do parenteses
+//Depois não consegui calcular a média quebrada, tinha usado int, e passei tudo para float e deu bom//// }
+
+}
+}
